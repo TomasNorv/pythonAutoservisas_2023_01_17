0dccaf8ab18593660e5f8312095c6d1a94bc6dd4
TomasNorvilis <tomasnorvilis@yahoo.com>
2023-01-18 15:07:55 +0200

    Padaryta, kad užsakymo formoje būtų matomos ir užsakymo eilutės (į jas galima būtų įrašyti informaciją)

--- mysite/autoservisas/admin.py
+++ mysite/autoservisas/admin.py
@@ -7,8 +7,14 @@ from .models import (Automobilis,
                      Uzsakymas,
                      UzsakymoEilute)
 
+class UzsakymoEiluteInLines(admin.TabularInline):
+    model = UzsakymoEilute
+    extra = 0
+class UzsakymasAdmin(admin.ModelAdmin):
+    inlines = [UzsakymoEiluteInLines]
+
 admin.site.register(Automobilis)
 admin.site.register(AutomobilioModelis)
 admin.site.register(Paslauga)
-admin.site.register(Uzsakymas)
+admin.site.register(Uzsakymas, UzsakymasAdmin)
 admin.site.register(UzsakymoEilute)
